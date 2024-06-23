7b46ee6099e37e8835b25b3db396bcc63d72db0a
francovaira <francogvaira@gmail.com>
2024-06-23 17:05:05 -0300

    improved ram_memory module & added testbench

--- src/ram_memory.v
+++ src/ram_memory.v
@@ -1,107 +1,63 @@
 `timescale 1ns / 1ps
 
-//  Xilinx Single Port No Change RAM
-//  This code implements a parameterizable single-port no-change memory where when data is written
-//  to the memory, the output remains unchanged.  This is the most power efficient write mode.
-//  If a reset or enable is not necessary, it may be tied off or removed from the code.
-
 module ram_memory
 #(
-    parameter RAM_WIDTH = 32,                                   // Specify RAM data width
-    parameter RAM_DEPTH = 2048,                                 // Specify RAM depth (number of entries)
-    parameter INIT_FILE = "" // Specify name/location of RAM initialization file if using one (leave blank if not)
+    parameter NB_DATA       = 32        ,
+    parameter RAM_DEPTH     = 256       ,   // RAM depth (number of entries)
+    parameter NB_ADDRESS    = 8             // Address bus length (depends on RAM_DEPTH)
 )
 (
-    input  wire [RAM_WIDTH-1:0] i_address   ,   // Address bus
-    input  wire                 i_valid     ,
-    input                       i_reset     ,
-    input                       i_clock     ,   // Clock
+    output wire [NB_DATA        -1 : 0] o_data              ,
 
-    output wire [RAM_WIDTH-1:0] o_instruction   // RAM output data
+    input  wire [NB_ADDRESS     -1 : 0] i_address           ,
+    input  wire [NB_DATA        -1 : 0] i_write_data        ,
+    input  wire                         i_write_enable      , // cada vez que hay un flanco positivo, se comienza a escribir desde la posicion 0
+    input  wire                         i_write_data_next   , // esta entrada recibe un pulso cada vez que se tiene que escribir en la proxima direccion
+    input                               i_reset             ,
+    input                               i_clock
 );
 
-    reg [RAM_WIDTH  -1 : 0] mem [0 : RAM_DEPTH];
-    // reg [RAM_WIDTH  -1:0] ram_data = {RAM_DEPTH{1'b0}};
+    reg [NB_DATA    -1 : 0] memory [0 : RAM_DEPTH-1];
+    reg [NB_ADDRESS -1 : 0] write_address;
+    reg                     write_enable;
 
-    always @(i_clock) begin
+    // --------------------------------------------------
+    // Incremental write address block
+    // --------------------------------------------------
+    always @(posedge i_clock) begin : wr_enable_block
         if(i_reset) begin
-            mem[0]  = 32'hCCDDEEFF;
-            mem[1]  = 32'h11223344;
-            mem[2]  = 32'hAABBCCDD;
-            mem[3]  = 32'h55667788;
-            mem[4]  = 32'hCCDDEEFF;
-            mem[5]  = 32'h11223344;
-            mem[6]  = 32'hAABBCCDD;
-            mem[7]  = 32'h55667788;
-            mem[8]  = 32'hCCDDEEFF;
-            mem[9]  = 32'h11223344;
-            mem[10] = 32'hAABBCCDD;
-            mem[11] = 32'h55667788;
-            mem[12] = 32'hCCDDEEFF;
-            mem[13] = 32'h11223344;
-            mem[14] = 32'hAABBCCDD;
-            mem[15] = 32'h55667788;
-            mem[16] = 32'hCCDDEEFF;
-            mem[17] = 32'h11223344;
-            mem[18] = 32'hAABBCCDD;
-            mem[19] = 32'h55667788;
-            mem[20] = 32'hCCDDEEFF;
-            mem[21] = 32'h11223344;
-            mem[22] = 32'hAABBCCDD;
-            mem[23] = 32'h55667788;
-            mem[24] = 32'hCCDDEEFF;
-            mem[25] = 32'h11223344;
-            mem[26] = 32'hAABBCCDD;
-            mem[27] = 32'h55667788;
-            mem[28] = 32'hCCDDEEFF;
-            mem[29] = 32'h11223344;
-            mem[30] = 32'hAABBCCDD;
-            mem[31] = 32'h55667788;
+            write_enable <= 1'b0;
+        end
+        else begin
+            write_enable <= i_write_enable;
         end
     end
 
-    // always @(*) begin
-    //     ram_data <= mem[i_address];
-    // end
+    always @(posedge i_clock) begin : incremental_wr_addr_block
+        if(i_reset || (~write_enable && i_write_enable)) begin
+            write_address <= 8'h0;
+        end
+        else if (write_enable && i_write_data_next) begin
+            write_address <= write_address + 8'h1;
+        end
+    end
 
-    assign o_instruction = mem[i_address];
+    // --------------------------------------------------
+    // Memory block
+    // --------------------------------------------------
+    always @(posedge i_clock) begin : wr_data_block
+        if(i_reset) begin
+            memory[0] <= 32'h0;
+        end
+        else if (write_enable && i_write_data_next) begin
+            memory[write_address] <= i_write_data;
+        end
+    end
 
 
-    // ##############
-
-    // wire                 enable       = 1; // RAM Enable, for additional power savings, disable port when not in use
-    // wire [RAM_WIDTH-1:0] input_data   = 0; // RAM input data
-    // wire                 reset        = 0; // Output reset (does not affect memory contents)
-    // wire                 reg_enable   = 1; // Output register enable
-
-    // reg [RAM_WIDTH  -1:0] PRAM [RAM_DEPTH -1:0];
-    // reg [RAM_WIDTH  -1:0] ram_data = {RAM_WIDTH{1'b0}};
-    // reg load_done = 1'b0;
-
-    // always@(*) begin: load_data_file
-    //     if(i_valid) begin
-    //         if (~load_done && INIT_FILE != "") begin
-    //             $readmemb(INIT_FILE, PRAM, 0, RAM_DEPTH-1);
-    //             load_done = 1'b1;
-    //         end
-    //     end
-    // end
-
-    // always @(*) begin
-    //     if (enable && i_valid) begin
-    //         ram_data <= PRAM[i_address];
-    //     end
-    // end
-
-    // // The following is a 1 clock cycle read latency at the cost of a longer clock-to-out timing
-    // assign o_instruction = ram_data;
-
-    // //  The following function calculates the address width based on specified RAM depth
-    // function integer clogb2;
-    //     input integer depth;
-    //         for (clogb2=0; depth>0; clogb2=clogb2+1) begin
-    //             depth = depth >> 1;
-    //         end
-    // endfunction
+    // --------------------------------------------------
+    // Output assignments
+    // --------------------------------------------------
+    assign o_data = memory[i_address];
 
 endmodule
